3ac3c1a11936c256fc0451ade8b95d887940bf00
chandan-0698 <137065359+chandan-0698@users.noreply.github.com>
2024-09-12 17:21:07 +0000

    DataTypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,21 @@
+"Use strict"; // treat all JS code as newer version
+
+//alert("hello") we are using nodeJS, not browser. alert is available but in different manner
+
+// console.log(""); code readability must be high
+
+let name = "Chandan"
+let age = 25
+let isLoggedIn = true
+
+// number = 2^52
+// bigint
+// string = ""
+// boolean = either true or false
+// null = standalone value
+// undefined = 
+// symbols = unique
+
+// object
+
+console.log(typeof null);
