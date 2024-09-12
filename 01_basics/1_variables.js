635ecda3e5f36d0e58a3fe1de38a17f4041af3d3
chandan-0698 <137065359+chandan-0698@users.noreply.github.com>
2024-09-12 17:11:34 +0000

    Variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountId = 15154 //const means once the value is locked, it can't be changed
+let accountEmail = "chandandhiman06@google.com" 
+var accountPassword = "123456"
+accountCity = "Himachal"
+let accountState;
+
+accountEmail = "dhimanchandan00@google.in"
+accountCity = "Jaipur"
+accountPassword = "09874"
+console.log("accountId");
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// don't use var 
